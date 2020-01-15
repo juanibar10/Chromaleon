cfedeb52272184fc4745958c15af89b9e38eacd4
unairf <GitLab4_>
2020-01-15 09:25:13 +0100

    Camera Shake

---
+++ Mov2D Concept/Assets/Scripts/Hoja.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoja : MonoBehaviour
+{
+    public Colores color;
+    public Material[] material;
+
+    private void Awake()
+    {
+        if (color == Colores.Azul)
+           GetComponent<MeshRenderer>().material = material[0];
+        else if (color == Colores.Rojo)
+           GetComponent<MeshRenderer>().material = material[1];
+        else if (color == Colores.Verde)
+           GetComponent<MeshRenderer>().material = material[2];
+    }
+}
